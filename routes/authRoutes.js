3d9f5c03c6f4b6e98de7441cb9e08e813f61ef31
tailordhruv24246-Btech <tailor.dhruv24246@jeckukas.org.in>
2026-03-12 23:21:08 +0530

    yes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,6 @@ router.post('/login', login);
 router.post('/reset-password', resetPassword);
 router.get('/me', authMiddleware, getMe);
 router.patch('/me', authMiddleware, updateMe);
+router.put('/me', authMiddleware, updateMe);
 
 module.exports = router;
